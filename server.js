034a4458d824b9932674b44a03670f48c397eb13
Artexis10 <hugoander@gmail.com>
2020-04-27 22:23:10 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/COVID-19', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(3000, ()=>{
+    console.log("Server is Running on Port 3000");
+});
