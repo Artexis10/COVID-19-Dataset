e2918d7b9b72bba558e5a3bf8b64e55f29a132ed
Artexis10 <hugoander@gmail.com>
2020-05-04 05:31:50 +0300

    Navigeerimisriba ja kodulehe täiendused

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/COVID-19', { useNewUrlParser: true, useUnifiedTopology: true });
 
-var $maxWidthElement = $('.navbar'),
-    maxWidth = $maxWidthElement.width();
-
-
-
 app.listen(3000, ()=>{
     console.log("Server is Running on Port 3000");
 });
