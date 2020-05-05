8a781996ec5bc4ea7250e4c63e7755384e0c3569
Artexis10 <hugoander@gmail.com>
2020-05-05 20:58:15 +0300

    Rakenduse täielik struktureerimine

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+require('dotenv/config');
 
-mongoose.connect('mongodb://localhost:27017/COVID-19', { useNewUrlParser: true, useUnifiedTopology: true });
+app.set('view engine', 'ejs');  
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, ()=>{
+// Import Routes
+const indexRoute = require('./routes/index');
+
+const countriesRoute = require('./routes/countries');
+
+app.use('/', indexRoute);
+app.use('/countries', countriesRoute);
+
+// Middlewares
+app.use('/', () => {
+    console.log('Middleware running');
+});
+
+// Connecting to database
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to AtlasDB!')
+);
+
+app.listen(3000, () => {
     console.log("Server is Running on Port 3000");
 });
 
