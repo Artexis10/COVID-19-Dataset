c8a7b2196b1d1f204df945b817f5e0f8bd8c21c7
Artexis10 <hugoander@gmail.com>
2020-05-05 21:01:40 +0300

    Rakenduse täielik struktureerimine

---
+++ routes/countries.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('countries', {
+        pageTitle: "Countries",
+        stylesheet: "countries",
+        activeHome: "nav-item pl-4 pl-md-0 ml-0 ml-md-4",
+        activeCountries: "nav-item pl-4 pl-md-0 ml-0 ml-md-4 active"
+    });
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('index', {
+        pageTitle: "COVID-19 Statistics",
+        stylesheet: "style",
+        activeHome: "nav-item pl-4 pl-md-0 ml-0 ml-md-4 active",
+        activeCountries: "nav-item pl-4 pl-md-0 ml-0 ml-md-4"
+    });
+});
+
+module.exports = router;
