95b25b78b3432f179d8fdf8254ecde9620162890
Artexis10 <hugoander@gmail.com>
2020-06-11 12:57:14 +0300

    Kujunduse ümbertegemine ja alalehekülgede täiustamine

---
+++ routes/graphs.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('graphs', {
+        pageTitle: "Graphs",
+        stylesheet: "graphs",
+        isActiveWorldwide: "inactive-link",
+        isActiveContinents: "inactive-link",
+        isActiveCountries: "inactive-link",
+        isActiveGraphs: "active-link"
+    });
+});
+
+module.exports = router;
