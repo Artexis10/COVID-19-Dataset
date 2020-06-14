12d353959d9cb5d298806c9b6806bc64b653a04f
Your Name <you@example.com>
2020-06-14 23:56:48 +0300

    kujunduse parandamine nutikasutajale

--- routes/worldwide.js
+++ routes/worldwide.js
@@ -1,21 +1,16 @@
 const express = require('express');
+const Continent = require('../models/Continent');
 const router = express.Router();
-const fetch = require("node-fetch");
 
 router.get('/', function(req, res) {
-    fetch('https://corona.lmao.ninja/v2/all?yesterday')
-    .then(response => response.json())
-    .then(global => {
         res.render('worldwide', {
             pageTitle: "Worldwide",
             stylesheet: "worldwide",
-            global: global,
             isActiveWorldwide: "active-link",
             isActiveContinents: "inactive-link",
             isActiveCountries: "inactive-link",
-            isActiveGraphs: "inactive-link"
+            isActiveGraphs: "inactive-link",
         });
     });
-});
 
 module.exports = router;
